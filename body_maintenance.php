0355cff3b8b2e0705d0bfc48fbf22e0adf3106f8
aminatwo <Administrateur@PC-V-29.via2s.local>
2015-08-05 10:22:17 +0200

    via2s
    tst

--- body_maintenance.php
+++ body_maintenance.php
@@ -1,3 +1,4 @@
+<html>
 <div class="col-sm-10" style="background-color: white; height: 80%">
 	<div class="container">
 		<h2 class="text_title" style="margin-left:35%">MAINTENANCE</h2>
@@ -16,3 +17,5 @@
 		</ul>
 	</div>
 </div>
+
+</html>
